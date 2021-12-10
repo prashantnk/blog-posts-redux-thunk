13040ba425b4cda7835dfe87154ee1976cfcbf55
prashant <prashantnk2001@gmail.com>
2021-12-10 23:53:26 +0530

    overfetching solution using memoize function of lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,29 @@
 import jsonPlaceHolder from '../api/jsonPlaceHolder';
+import _ from 'lodash';
 export const fetchPosts = () => async dispatch => {
     const result = await jsonPlaceHolder.get("/posts");
     dispatch({ type: "FETCH_POSTS", payload: result.data });
 }
 
-export const fetchUser = (id) => async dispatch => {
+// export const fetchUser = (id) => async dispatch => {
+//     const result = await jsonPlaceHolder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: result.data });
+// }
+
+
+//  solution using memoization 
+const temp = _.memoize(async (id, dispatch) => {
     const result = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({ type: "FETCH_USER", payload: result.data });
+});
+export const fetchUser = (id) => {
+    return ((dispatch) => {
+        temp(id, dispatch);
+    });
 }
+
+
+
 
 
 
